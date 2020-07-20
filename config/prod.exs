7823cd6669566b47d0f76d8d1122c5c2321ce766
Justin Tormey <jrtormey@gmail.com>
2020-07-20 14:35:23 -0500

    Configure production url

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :linear, LinearWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: "linear-sync.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
