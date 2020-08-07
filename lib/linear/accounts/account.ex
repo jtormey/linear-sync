79b2a055bb0a462c31a9f5ac891dd849ebb8f9c1
Justin Tormey <jrtormey@gmail.com>
2020-08-07 14:00:28 -0500

    Validate that api key works before creating account

--- lib/linear/accounts/account.ex
+++ lib/linear/accounts/account.ex
@@ -2,6 +2,8 @@ defmodule Linear.Accounts.Account do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias Linear.LinearAPI
+
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
 
@@ -18,6 +20,22 @@ defmodule Linear.Accounts.Account do
     account
     |> cast(attrs, [:api_key])
     |> validate_required([:api_key])
+    |> validate_linear_api_key(:api_key)
     |> unique_constraint(:api_key, name: :accounts_api_key_index)
+  end
+
+  def validate_linear_api_key(changeset, field) do
+    validate_change changeset, field, fn ^field, value ->
+      value
+      |> LinearAPI.Session.new()
+      |> LinearAPI.viewer()
+      |> case do
+        {:ok, %{"data" => %{"viewer" => _viewer}}} ->
+          []
+
+        _otherwise ->
+          [{field, "unable to authenticate Linear api key"}]
+      end
+    end
   end
 end
