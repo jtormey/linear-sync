183fad8b52540026e682e2f3f0632661d2091c6e
Justin Tormey <jrtormey@gmail.com>
2021-03-31 15:48:21 -0500

    Disable unused linear webhook integration

--- lib/linear/issue_sync_service.ex
+++ lib/linear/issue_sync_service.ex
@@ -5,14 +5,14 @@ defmodule Linear.IssueSyncService do
   alias Linear.Accounts.Account
   alias Linear.Data
   alias Linear.Data.IssueSync
-  alias Linear.LinearAPI
+  # alias Linear.LinearAPI
   alias Linear.GithubAPI
   alias LinearWeb.Router.Helpers, as: Routes
 
   def enable_issue_sync(account = %Account{id: id}, issue_sync = %IssueSync{account_id: id, enabled: false}) do
     Ecto.Multi.new()
     |> Ecto.Multi.update(:issue_sync, Data.change_issue_sync(issue_sync, %{enabled: true}))
-    |> Ecto.Multi.run(:linear_webhook, fn repo, multi -> enable_linear_webhook(account, repo, multi) end)
+    # |> Ecto.Multi.run(:linear_webhook, fn repo, multi -> enable_linear_webhook(account, repo, multi) end)
     |> Ecto.Multi.run(:github_webhook, fn repo, multi -> enable_github_webhook(account, repo, multi) end)
     |> Repo.transaction()
     |> handle_result()
@@ -21,44 +21,44 @@ defmodule Linear.IssueSyncService do
   def disable_issue_sync(account = %Account{id: id}, issue_sync = %IssueSync{account_id: id, enabled: true}) do
     Ecto.Multi.new()
     |> Ecto.Multi.update(:issue_sync, Data.change_issue_sync(issue_sync, %{enabled: false}))
-    |> Ecto.Multi.run(:linear_webhook, fn repo, multi -> disable_linear_webhook(account, repo, multi) end)
+    # |> Ecto.Multi.run(:linear_webhook, fn repo, multi -> disable_linear_webhook(account, repo, multi) end)
     |> Ecto.Multi.run(:github_webhook, fn repo, multi -> disable_github_webhook(account, repo, multi) end)
     |> Repo.transaction()
     |> handle_result()
   end
 
-  def enable_linear_webhook(account, repo, multi) do
-    result = LinearAPI.create_webhook LinearAPI.Session.new(account),
-      url: Routes.linear_webhook_url(LinearWeb.Endpoint, :handle),
-      teamId: multi.issue_sync.team_id
+  # def enable_linear_webhook(account, repo, multi) do
+  #   result = LinearAPI.create_webhook LinearAPI.Session.new(account),
+  #     url: Routes.linear_webhook_url(LinearWeb.Endpoint, :handle),
+  #     teamId: multi.issue_sync.team_id
+  #
+  #   case result do
+  #     {:ok, %{"data" => %{"webhookCreate" => %{"success" => true, "webhook" => %{"id" => webhook_id, "enabled" => true}}}}} ->
+  #       multi.issue_sync
+  #       |> Data.change_issue_sync(%{linear_webhook_id: webhook_id})
+  #       |> repo.update()
+  #
+  #     error ->
+  #       Logger.error("Failed to enable Linear webhook, #{inspect error}")
+  #       {:error, :linear_webhook_enable_failure}
+  #   end
+  # end
 
-    case result do
-      {:ok, %{"data" => %{"webhookCreate" => %{"success" => true, "webhook" => %{"id" => webhook_id, "enabled" => true}}}}} ->
-        multi.issue_sync
-        |> Data.change_issue_sync(%{linear_webhook_id: webhook_id})
-        |> repo.update()
-
-      error ->
-        Logger.error("Failed to enable Linear webhook, #{inspect error}")
-        {:error, :linear_webhook_enable_failure}
-    end
-  end
-
-  def disable_linear_webhook(account, repo, multi) do
-    result = LinearAPI.delete_webhook LinearAPI.Session.new(account),
-      id: multi.issue_sync.linear_webhook_id
-
-    case result do
-      {:ok, %{"data" => %{"webhookDelete" => %{"success" => true}}}} ->
-        multi.issue_sync
-        |> Data.change_issue_sync(%{linear_webhook_id: nil})
-        |> repo.update()
-
-      error ->
-        Logger.error("Failed to disable Linear webhook, #{inspect error}")
-        {:error, :linear_webhook_disable_failure}
-    end
-  end
+  # def disable_linear_webhook(account, repo, multi) do
+  #   result = LinearAPI.delete_webhook LinearAPI.Session.new(account),
+  #     id: multi.issue_sync.linear_webhook_id
+  #
+  #   case result do
+  #     {:ok, %{"data" => %{"webhookDelete" => %{"success" => true}}}} ->
+  #       multi.issue_sync
+  #       |> Data.change_issue_sync(%{linear_webhook_id: nil})
+  #       |> repo.update()
+  #
+  #     error ->
+  #       Logger.error("Failed to disable Linear webhook, #{inspect error}")
+  #       {:error, :linear_webhook_disable_failure}
+  #   end
+  # end
 
   def enable_github_webhook(account, repo, multi) do
     client = GithubAPI.client(account)
