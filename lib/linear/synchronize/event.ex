7e4d951ab198daa03b695d19100c4f6ef152d91e
Justin Tormey <jrtormey@gmail.com>
2021-10-17 08:36:55 -0500

    Implement standard event struct

---
+++ lib/linear/synchronize/event.ex
@@ -0,0 +1,33 @@
+defmodule Linear.Synchronize.Event do
+  alias __MODULE__
+
+  @enforce_keys [
+    :source,
+    :action,
+    :data
+  ]
+
+  defstruct [
+    :source,
+    :action,
+    :repo_id,
+    :team_id,
+    :issue_sync_id,
+    data: %{}
+  ]
+
+  @doc """
+  Returns the possible attrs for an event.
+  """
+  def attrs(%Event{} = event) do
+    %{
+      github_issue_id: get_in(event.data, [:github_issue, :id]),
+      github_issue_number: get_in(event.data, [:github_issue, :number]),
+      github_comment_id: get_in(event.data, [:github_comment, :id]),
+      linear_issue_id: get_in(event.data, [:linear_issue, :id]),
+      linear_issue_number: get_in(event.data, [:linear_issue, :number]),
+      linear_comment_id: get_in(event.data, [:linear_comment, :id]),
+      issue_sync_id: event.issue_sync_id
+    }
+  end
+end
