04d412429eb152be1209dd23749f98189039426b
Justin Tormey <jrtormey@gmail.com>
2021-10-17 18:58:27 -0500

    Fix access error in event

--- lib/linear/synchronize/event.ex
+++ lib/linear/synchronize/event.ex
@@ -17,14 +17,14 @@ defmodule Linear.Synchronize.Event do
   @doc """
   Returns the possible attrs for an event.
   """
-  def attrs(%Event{} = event) do
+  def attrs(%Event{data: data} = event) do
     %{
-      github_issue_id: get_in(event.data, [:github_issue, :id]),
-      github_issue_number: get_in(event.data, [:github_issue, :number]),
-      github_comment_id: get_in(event.data, [:github_comment, :id]),
-      linear_issue_id: get_in(event.data, [:linear_issue, :id]),
-      linear_issue_number: get_in(event.data, [:linear_issue, :number]),
-      linear_comment_id: get_in(event.data, [:linear_comment, :id]),
+      github_issue_id: data[:github_issue] && data.github_issue.id,
+      github_issue_number: data[:github_issue] && data.github_issue.number,
+      github_comment_id: data[:github_comment] && data.github_comment.id,
+      linear_issue_id: data[:linear_issue] && data.linear_issue.id,
+      linear_issue_number: data[:linear_issue] && data.linear_issue.number,
+      linear_comment_id: data[:linear_comment] && data.linear_comment.id,
       issue_sync_id: event.issue_sync_id
     }
   end
